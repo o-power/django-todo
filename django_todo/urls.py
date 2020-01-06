2d99b82ac0d567b999f92de836a794416fb3e4b6
Ollie Power <powerollie@gmail.com>
2020-01-06 22:11:06 +0000

    Added view and sqlite db

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from todo.views import say_hello
+from todo.views import say_hello, get_todo_list
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'message', say_hello)
+    url(r'message', say_hello),
+    url(r'^$', get_todo_list)
 ]
