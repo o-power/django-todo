fe9b08a47958402c8779f86a5b059c44a67ce6ed
Ollie Power <powerollie@gmail.com>
2020-01-09 22:37:16 +0000

    Adding models

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# A single dot means that the module or package referenced is in the same directory as the current location. 
+from .models import Item
 
 # Register your models here.
+admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# Each model is represented by a class
+class Item(models.Model):
+    # Each database field is represented by a class variable
+    name = models.CharField(max_length=30, blank=False)
+    done = models.BooleanField(blank=False, default=False)
+
+    def __str__(self):
+        return self.name
