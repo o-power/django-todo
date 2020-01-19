03b6673d9f083fc3254129040749d3c1d1e0ab82
Ollie Power <powerollie@gmail.com>
2020-01-19 14:06:51 +0000

    Adding Procfile and env variables to settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'zd9+uigg^7k+i+qy_dizq_t%4nu7j*a$*t(^44pzem9zbb9oi5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ollies-django-todo.herokuapp.com']
 
 
 # Application definition
@@ -75,13 +76,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse("postgres://vewsnpdohoeawy:9c134638ae869e7e28980bae7cc9c08437d9cc48fb5d49d4aa16363c77453fde@ec2-46-137-187-23.eu-west-1.compute.amazonaws.com:5432/d5ikiu65h8rfdc")
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
